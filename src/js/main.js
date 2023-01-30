b20c06018c7c43de405832ef5aa875822c4e1430
Amadoubenthe <amadbenthebagnandiallo@gmail.com>
2023-01-30 16:58:22 +0100

    Add delete

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,42 @@
 const btnUpdate = document.querySelector(".btn-update");
+const btnDelete = document.querySelector(".btn-delete");
+const input = document.getElementById("input");
 
-async function getTaskById(id) {
-  await fetch(`http://localhost:3000/taches/${id}`)
-    .then((res) => res.json())
-    .then((task) => console.log(task));
+async function updateTask(id) {
+  let title = "json-server Brice";
+  const res = await fetch("http://localhost:3000/taches/" + id, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+
+    body: JSON.stringify({ title }),
+  });
 }
 
 btnUpdate.addEventListener("click", function () {
-  getTaskById(1);
+  updateTask(3);
 });
+
+btnDelete.addEventListener("click", function () {
+  deleteTask(1);
+});
+
+/**
+ * Delete a task
+ * @param {number} taskId
+ * @returns
+ */
+
+const deleteTask = (taskId) => {
+  if (!taskId) return;
+
+  const options = {
+    method: "DELETE",
+    headers: new Headers(),
+  };
+
+  fetch(`http://localhost:3000/taches/${taskId}`, options).catch((err) =>
+    console.error(err)
+  );
+};
